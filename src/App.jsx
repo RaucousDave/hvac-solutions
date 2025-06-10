aab02f7a77d26dfce17ca7165ae6a4e1ba992d1d
Davies Enienghan <daviesenienghan906@gmail.com>
2025-06-10 07:07:26 +0100

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import About from "./components/About";
 import Cta from "./components/Cta";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
 import Testimonials from "./components/Testimonials";
